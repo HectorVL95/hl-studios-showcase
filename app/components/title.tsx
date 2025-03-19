4c8c36df1cc52df8707c7d19afbbe4835c6ae5c3
Hector VL <francisco1495@gmail.com>
2025-03-19 12:23:06 -0700

    progress

--- app/components/title.tsx
+++ app/components/title.tsx
@@ -3,9 +3,9 @@ import { TitleProps } from "../types/children-props-types";
 const TitlenSubtitle: React.FC<TitleProps> = ({ title, subtitle }) => {
   return (
     <>
-    <h3 className="text-6xl font-bold">{title}</h3>
+    <h3 className="text-6xl">{title}</h3>
     { 
-      subtitle && <p className="text-[32px] align-middle font-semibold">{subtitle}</p>
+      subtitle && <p className="text-[32px] align-middle">{subtitle}</p>
     }
 
     </>
--- app/layouts/homepage/components/banner.tsx
+++ app/layouts/homepage/components/banner.tsx
@@ -6,7 +6,7 @@ const Banner: React.FC<BannerProps> = ({text, img}) => {
 
   return (
       <div className="bg-[url('/images/${img}')] bg-cover bg-center bg-no-repeat w-full h-[900px] flex justify-center items-center" style={{backgroundImage: `url('/images/${img}')`}}>
-      {text && <p className="font-bold text-lg sm:2xl md:text-4xl">{text}</p>}
+      {text && <p className="uppercase text-7xl sm:2xl md:text-4xl">{text}</p>}
       </div>
   );
 }
