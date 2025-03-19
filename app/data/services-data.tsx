01640f981101f9e770b7a068e55dae8e931d1506
Hector Vl <francisco1495@gmail.com>
2025-03-18 17:04:28 -0700

    working on services data

--- app/data/services-data.tsx
+++ app/data/services-data.tsx
@@ -3,9 +3,64 @@
 const services_data = [
   {
     id: 0,
-    img: '',
-    title: '',
+    img: '/images/service-wedding.png',
+    title: 'Weddings',
     services_card: [
+      {
+        title:'',
+        price: '',
+        services: [
+          '100 Edited photos',
+          '1 Photographer',
+          '1 Videographer',
+          '1 Wedding Film (15 to 30 min)',
+          '1 Short Highlights (1 min)',
+          'Digital Gallery',
+          "Crystal USB Drive with all the event content delivered in a custom wooden box engraved with the bride and groom's name.",
+        ]
+      },
+      {
+        title:'',
+        price: '',
+        services: [
+          '6 hours coverage',
+          '200 Edited photos',
+          '1 Photographer',
+          '2 Videographers',
+          '1 Wedding Film (15 to 45 min)',
+          '1 Short Highlights (1 min)',
+          'Digital Gallery',
+          '2 Printed Bridal Portraits for reception 16”x20”',
+          "Crystal USB Drive with all the event content delivered in a custom wooden box engraved with the bride and groom's name.Content delivered within 3 business weeks"
+        ]
+      },
+      {
+        title:'',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      }
+    ]
+  },
+  {
+    id: 1,
+    img: '/images/service-wedding.png',
+    title: 'XVs Celebrations',
+    services_card: [
+      {
+        title:'Silver Wedding Collection',
+        price: 'CA $799',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      },
       {
         title:'',
         price: '',
@@ -16,23 +71,16 @@ const services_data = [
           '',
         ]
       },
-      {},
-      {}
+      {
+        title:'',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      }
     ]
-  },
-  {
-
-  },
-  {
-
-  },
-  {
-
-  },
-  {
-
-  },
-  {
-
   },
 ]
