ebd9ee6765e9618031621b52d092116b550487f4
Hector Vl <francisco1495@gmail.com>
2025-03-19 16:36:04 -0700

    progress

--- app/data/services-data.tsx
+++ app/data/services-data.tsx
@@ -7,21 +7,23 @@ const services_data = [
     title: 'Weddings',
     services_card: [
       {
-        title:'',
-        price: '',
+        title:'Silver wedding collection',
+        price: 'CA $799',
         services: [
+          '4 hourd coverage',
           '100 Edited photos',
           '1 Photographer',
           '1 Videographer',
           '1 Wedding Film (15 to 30 min)',
           '1 Short Highlights (1 min)',
           'Digital Gallery',
-          "Crystal USB Drive with all the event content delivered in a custom wooden box engraved with the bride and groom's name.",
+          "Crystal USB Drive with all the event content delivered in a custom wooden box engraved with the bride and groom's name.",,
+          'Content delivered within 2 business weeks'
         ]
       },
       {
-        title:'',
-        price: '',
+        title:'Golden wedding collection',
+        price: 'CA $1200',
         services: [
           '6 hours coverage',
           '200 Edited photos',
@@ -31,7 +33,57 @@ const services_data = [
           '1 Short Highlights (1 min)',
           'Digital Gallery',
           '2 Printed Bridal Portraits for reception 16”x20”',
-          "Crystal USB Drive with all the event content delivered in a custom wooden box engraved with the bride and groom's name.Content delivered within 3 business weeks"
+          "Crystal USB Drive with all the event content delivered in a custom wooden box engraved with the bride and groom's name.Content delivered within 3 business weeks",
+          'Content delivered within 3 business weeks'
+        ]
+      },
+      {
+        title:'Platino wedding collection',
+        price: 'CA $1899',
+        services: [
+          '8 hours coverage',
+          '300 Edited photos',
+          '1 Photographer',
+          '2 Videographers',
+          'Drone Coverage for Ceremony and Reception party',
+          '1 Wedding Film (15 to 60 min)',
+          '1 Short Highlights (1 min)',
+          'Digital Gallery',
+          '6 Printed Bridal Portraits for reception 16”x20”',
+          "Crystal USB Drive with all the event content delivered in a custom wooden box engraved with the bride and groom's name",
+          'Content delivered within 3 business weeks'
+        ]
+      }
+    ]
+  },
+  {
+    id: 1,
+    img: '/images/service-xv.png',
+    title: 'XVs Celebrations',
+    services_card: [
+      {
+        title:'Silver XV Collection',
+        price: 'CA $799',
+        services: [
+          '4 hours coverage',
+          '100 Edited photos',
+          '1 Photographer',
+          '1 Videographer',
+          '1 XV film (15 to 30 min)',
+          '1 Short Highlights (1 min)',
+          'Digital Gallery',
+          'Crystal USB Drive with all the event content delivered in a custom wooden box engraved with the Quinceañera’s name',
+          'Content delivered within 2 business weeks'
+        ]
+      },
+      {
+        title:'Golden XV collection',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
         ]
       },
       {
@@ -47,13 +99,124 @@ const services_data = [
     ]
   },
   {
-    id: 1,
-    img: '/images/service-wedding.png',
+    id: 2,
+    img: '/images/service-bd.png',
     title: 'XVs Celebrations',
     services_card: [
       {
-        title:'Silver Wedding Collection',
-        price: 'CA $799',
+        title:'',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      },
+      {
+        title:'',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      },
+      {
+        title:'',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      }
+    ]
+  },
+  {
+    id: 3,
+    img: '/images/service-maternity.png',
+    title: 'XVs Celebrations',
+    services_card: [
+      {
+        title:'',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      },
+      {
+        title:'',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      },
+      {
+        title:'',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      }
+    ]
+  },
+  {
+    id: 4,
+    img: '/images/service-commercial.png',
+    title: 'XVs Celebrations',
+    services_card: [
+      {
+        title:'',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      },
+      {
+        title:'',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      },
+      {
+        title:'',
+        price: '',
+        services: [
+          '',
+          '',
+          '',
+          '',
+        ]
+      }
+    ]
+  },
+  {
+    id: 5,
+    img: '/images/service-custom.png',
+    title: 'XVs Celebrations',
+    services_card: [
+      {
+        title:'',
+        price: '',
         services: [
           '',
           '',
